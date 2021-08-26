8e4324ce9cd0e3c87a69c08f7ed47b4cec215c55
Ichunjo <ichunjo.le.terrible@gmail.com>
2021-08-26 23:16:16 +0200

    add image module

---
+++ pyonfx/image.py
@@ -0,0 +1,10 @@
+
+from typing import NoReturn
+
+
+class Image:
+    def image_to_ass(self) -> NoReturn:
+        raise NotImplementedError
+
+    def image_to_pixels(self) -> NoReturn:
+        raise NotImplementedError
