4bf256ebea5f39eb14515cb0f601f549778d59da
Ichunjo <ichunjo.le.terrible@gmail.com>
2021-09-18 04:32:28 +0200

    add geometry objects to package `__all__`

--- pyonfx/__init__.py
+++ pyonfx/__init__.py
@@ -1,9 +1,9 @@
 # flake8: noqa
-
 from .ass import *
 from .colourspace import *
 from .convert import *
 from .core import *
+from .geometry import CartesianAxis, Geometry, PolarAxis
 from .image import *
 from .shape import *
 from .utils import *
